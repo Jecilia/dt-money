6fb4fdeb00fc52a42f66ed0df3527e2a030a669f
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-06-10 00:32:43 +0100

    accessible radio button, UI finalization

--- src/components/new-transaction-modal/index.tsx
+++ src/components/new-transaction-modal/index.tsx
@@ -21,11 +21,11 @@ export function NewTransactionModal() {
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
           <TransactionType>
-            <TransactionTypeButton variant="income">
+            <TransactionTypeButton variant="income" value="income">
               <ArrowCircleUp size={24} />
               Entrada
             </TransactionTypeButton>
-            <TransactionTypeButton variant="outcome">
+            <TransactionTypeButton variant="outcome" value="outcome">
               <ArrowCircleDown size={24} />
               Saida
             </TransactionTypeButton>
