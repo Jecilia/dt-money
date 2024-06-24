fce8e89f8f0033e0692ba88c0dac28716d9c5ad3
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-06-24 23:24:28 +0100

    ...

--- src/pages/transactions/components/search-form/index.tsx
+++ src/pages/transactions/components/search-form/index.tsx
@@ -1,10 +1,24 @@
 import { MagnifyingGlass } from 'phosphor-react'
 import { SearchFormContainer } from './styles'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
 
+const searshFormSchema = z.object({
+  query: z.string(),
+})
+type SearchFormInputs = z.infer<typeof searshFormSchema>  
 export function SearchForm() {
+  const { register, handleSubmit } = useForm<SearchFormInputs>()
+  function handleSearchTransactions(){
+    
+  }
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
       <button type="submit">
         <MagnifyingGlass size={20} />
         Buscar
